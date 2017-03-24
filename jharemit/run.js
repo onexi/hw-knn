12f2975062ce71c8233b34eea885c3e621965c1d
jharemit <jbhare@mit.edu>
2017-03-24 02:14:11 +0100

    PS5

---
+++ jharemit/run.js
@@ -0,0 +1,21 @@
+var exercise = require('./exercise.js');
+
+var data = exercise.one();
+//console.log(data);
+console.log(data.length);
+
+var data = exercise.two(data);
+//console.log(data);
+console.log(data.length);
+
+var data = exercise.three(data);
+console.log(data);
+//console.log(Object.keys(data).length);
+
+var data = exercise.four(data);
+console.log(data);
+// console.log(Object.keys(data).length);
+
+var data = exercise.five(data);
+console.log(data);
+// console.log(data.test.correct + ' correct out of ' + data.test.length);
