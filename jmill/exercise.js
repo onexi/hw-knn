ffb6419d906aee16db3d8402f1444ebbcd7def9d
JMill <Jonathan@JMillville.com>
2017-03-23 22:11:46 -0400

    Convert raw data into structure array

--- jmill/exercise.js
+++ jmill/exercise.js
@@ -5,14 +5,31 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 1st function not implemented';
+    // JM added here:
+    var digitstructure = [];
+    var trimput = input.replace(/ /g,''); //remove whitespace
+    
+    while (trimput.indexOf(',') != -1){
+        var commaIndex = trimput.indexOf(',');
+        var onesAndZeroes = trimput.slice(0,commaIndex);
+        var theLabel = trimput.slice(commaIndex+1, commaIndex+2);
+        console.log(theLabel);
+        digitstructure.push({
+            label: theLabel,
+            digits: onesAndZeroes
+        });
+        trimput = trimput.slice(commaIndex+3, trimput.length);
+    };
+    return digitstructure;
+
 };
 
 exercise.two = function(data){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 2nd function not implemented';
+    // JM added here:
+    
 };
 
 exercise.three = function(data){
