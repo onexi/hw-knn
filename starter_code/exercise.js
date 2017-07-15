018a654321e37566b2f4c9d6bc82f7d7c8da8848
yinxingmaimingg <yinxingmaimingg@hotmail.com>
2017-07-15 09:54:20 +0800

    Updated exercise file(s)

--- starter_code/exercise.js
+++ starter_code/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: hello';
+    return 'Error: brant test 1';
 };
 
 exercise.two = function(data){
