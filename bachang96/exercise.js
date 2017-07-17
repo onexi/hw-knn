b9ae269ca249d83695c65826bf1eba502f0435db
bachang96 <bachang96@hotmail.com>
2017-07-17 19:23:13 -0400

    Updated exercise file(s)

--- bachang96/exercise.js
+++ bachang96/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: wheee success';
+    return 'Error: testing';
 };
 
 exercise.two = function(data){
