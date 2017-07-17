6cf6f760d4b6192e6e2a3b10eaf06201d221951b
derekmma <hi@derek.ma>
2017-07-17 12:10:59 -0400

    Updated exercise file(s)

--- starter_code/exercise.js
+++ starter_code/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: hello';
+    return 'Error: hello 1210';
 };
 
 exercise.two = function(data){
