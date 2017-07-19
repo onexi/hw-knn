ba1b87bbb930ffb292135c9c770e426c4e88787f
derekmma <hi@derek.ma>
2017-07-19 18:21:56 -0400

    Updated exercise file(s)

--- derekmma/exercise.js
+++ derekmma/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 1st function not implemented';
+    return '1821';
 };
 
 exercise.two = function(data){
