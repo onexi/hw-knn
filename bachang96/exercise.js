1cdf63c9b97317b3797360db8239bcb60788a2eb
bachang96 <bachang96@hotmail.com>
2017-07-19 20:43:18 -0400

    Updated exercise file(s)

--- bachang96/exercise.js
+++ bachang96/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 1st function not implemented';
+    return 'Error: 1st function not implementest';
 };
 
 exercise.two = function(data){
