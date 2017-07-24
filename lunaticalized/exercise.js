b4c001b44572f17eafb84d7badc6958157a9f3eb
lunaticalized <blah@test.com>
2017-07-25 06:33:58 +0800

    testing

--- lunaticalized/exercise.js
+++ lunaticalized/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 1st function not implemented';
+    return 'Error: asdf function not implemented';
 };
 
 exercise.two = function(data){
