4706b10efd736d4438a9f6efd49428944ead7864
bachang96 <bachang96@hotmail.com>
2017-07-27 11:53:22 -0400

    Updated exercise file(s)

--- bachang96/exercise.js
+++ bachang96/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: pls work';
+    return 'Error: yay finally';
 };
 
 exercise.two = function(data){
