da3d0b773f3521ca5e68b98cb1c9ce03b562b69e
bachang96 <bachang96@hotmail.com>
2017-07-31 20:30:55 -0400

    Update exercise.js

--- starter_code/exercise.js
+++ starter_code/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 1st function not implemented';
+    return 'Test update';
 };
 
 exercise.two = function(data){
