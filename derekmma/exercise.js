4e0e9cd6ffc2148cb74ec0c95c1b24534d481a0f
Derek Mingyu MA <hi@derek.ma>
2017-08-07 17:51:11 -0400

    Update exercise.js

--- derekmma/exercise.js
+++ derekmma/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Test 1823';
+    return 'Test 1751';
 };
 
 exercise.two = function(data){
