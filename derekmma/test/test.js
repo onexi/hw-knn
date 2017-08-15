72c7c26ec77cdf2893d51e2a7317d5564c5b73ae
derekmma <hi@derek.ma>
2017-08-15 14:11:37 -0400

    Updated exercise file(s)

--- derekmma/test/test.js
+++ derekmma/test/test.js
@@ -2,19 +2,19 @@ var expect = require('chai').expect;
 var ex = require('../exercise.js');
 
 describe('There are no tests for this HW', function() {
-	it('local no tests available', function() {
+	it('local2 no tests available', function() {
 	    expect(1).to.equal(1);
 	});
-	it('local testing', function() {
+	it('local2 testing', function() {
 	    expect(1).to.equal(2);
 	});
 });
 
 describe('Mock test', function() {
-	it('local should fail', function() {
+	it('local2 should fail', function() {
 	    expect(2).to.equal(1);
 	});
-	it('local should pass', function() {
+	it('local2 should pass', function() {
 	    expect(2).to.equal(2);
 	});
 });
