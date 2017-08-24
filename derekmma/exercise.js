95666aa25f29ab2d899dcf18f48a1b2ad5954c16
Derek Mingyu MA <hi@derek.ma>
2017-08-24 00:13:06 -0400

    Update exercise.js

--- derekmma/exercise.js
+++ derekmma/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 1st function not implemented 0008';
+    return 'Error: 1st function not implemented 0012';
 };
 
 exercise.two = function(data){
