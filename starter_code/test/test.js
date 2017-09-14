a123aa5467559d961979dc0c42b0f2b9bd06a91e
bachang96 <bachang96@hotmail.com>
2017-09-14 12:45:13 +0800

    Readded chai dependency

--- starter_code/test/test.js
+++ starter_code/test/test.js
@@ -1,39 +1,20 @@
-// var expect = require('chai').expect;
-var assert = require('assert');
+var expect = require('chai').expect;
 var ex = require('../exercise.js');
 
 describe('There are no tests for this HW', function() {
 	it('no tests available', function() {
-	    assert.deepEqual(1, 1);
+	    expect(1).to.equal(1);
 	});
 	it('testing', function() {
-	    assert.deepEqual(1, 2);
+	    expect(1).to.equal(2);
 	});
 });
 
 describe('Mock test', function() {
 	it('should fail', function() {
-	    assert.deepEqual(2, 1);
+	    expect(2).to.equal(1);
 	});
 	it('should pass', function() {
-	    assert.deepEqual(2, 2);
+	    expect(2).to.equal(2);
 	});
 });
-
-// describe('There are no tests for this HW', function() {
-// 	it('no tests available', function() {
-// 	    expect(1).to.equal(1);
-// 	});
-// 	it('testing', function() {
-// 	    expect(1).to.equal(2);
-// 	});
-// });
-
-// describe('Mock test', function() {
-// 	it('should fail', function() {
-// 	    expect(2).to.equal(1);
-// 	});
-// 	it('should pass', function() {
-// 	    expect(2).to.equal(2);
-// 	});
-// });
