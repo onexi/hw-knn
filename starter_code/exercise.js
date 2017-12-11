17bb61eda9e8e86aa447e85f569cb9638aadbc2f
dkma <imamingyu@gmail.com>
2017-12-11 17:40:18 +0800

    Update exercise.js

--- starter_code/exercise.js
+++ starter_code/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 1st function not implemented';
+    return '1740'
 };
 
 exercise.two = function(data){
