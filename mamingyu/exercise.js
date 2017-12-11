43dd24a265a4d4eb8ca625ba5422677d2a79231b
mamingyu <imamingyu@gmail.com>
2017-12-11 21:42:34 +0800

    Updated exercise file(s)

--- mamingyu/exercise.js
+++ mamingyu/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return '2139';
+    return '2142';
 };
 
 exercise.two = function(data){
