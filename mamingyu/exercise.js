e2b751764dc527ded3f8be85bca877cefe62b5d9
mamingyu <imamingyu@gmail.com>
2017-12-11 20:21:00 +0800

    Updated exercise file(s)

--- mamingyu/exercise.js
+++ mamingyu/exercise.js
@@ -5,7 +5,7 @@ exercise.one = function(){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return '2019';
+    return '2020';
 };
 
 exercise.two = function(data){
