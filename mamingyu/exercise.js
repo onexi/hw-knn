dd352e53c817f8cb76f41a697ce21902b9847d30
dkma <imamingyu@gmail.com>
2018-02-12 20:43:29 +0800

    Update exercise.js

--- mamingyu/exercise.js
+++ mamingyu/exercise.js
@@ -6,7 +6,7 @@ exercise.one = function(){
     //---- Your Code ----
     //-------------------
     console.log('test0908');
-    return '1900';
+    return '2044';
 };
 
 exercise.two = function(data){
