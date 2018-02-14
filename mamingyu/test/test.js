13442521c368a6e960f02ba13172346aa8135abb
mamingyu <imamingyu@gmail.com>
2018-02-14 19:06:44 +0800

    Updated exercise file(s)

--- mamingyu/test/test.js
+++ mamingyu/test/test.js
@@ -6,13 +6,13 @@ describe('There are no tests for this HW', function() {
 	    expect(1).to.equal(1);
 	});
 	it('testing', function() {
-	    expect(1).to.equal(2);
+	    expect(1).to.equal(1);
 	});
 });
 
 describe('Mock test', function() {
 	it('should fail', function() {
-	    expect(2).to.equal(1);
+	    expect(2).to.equal(2);
 	});
 	it('should pass', function() {
 	    expect(2).to.equal(2);
